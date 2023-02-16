dc3e0f979ab2443ab0eb6f1c385ed4f78c4fcc7f
EduardSanosian2003 <93383924+EduardSanosian2003@users.noreply.github.com>
2023-02-16 14:45:49 +0100

    LuckyController

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -4,19 +4,28 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyController extends AbstractController
 {
-
     public function number(): Response
     {
         $number = random_int(0, 100);
 
-        return $this->render('bezoeker/number.html.twig',  ['number'=>$number]);
-
-
         return new Response(
-
+            '<html><body>Lucky number: '.$number.'</body></html>'
         );
     }
+
+    #[Route('/home')]
+    public function showHome(){
+        return $this->render("bezoeker/home.html.twig");
+
+        }
+    #[Route('/bestel')]
+    public function showBestllen(){
+
+        return $this->render('bezoeker/bestel.html.twig');
+
+        }
 }
